052a65d67c8c956b5472e2b6d37b3fa5fdba24af
yuvraj <yuvrajahuja4@gmail.com>
2024-10-14 14:42:35 +0530

    fixed error

--- client/src/components/DarkMode.tsx
+++ client/src/components/DarkMode.tsx
@@ -2,13 +2,16 @@ import {Button} from "@/components/ui/button.tsx";
 import {Sun, Moon} from "lucide-react";
 import {useState} from "react";
 
-const [theme, setTheme] = useState('light');
+
+
+export function DarkButton(){
 
 const toggleTheme = () => {
     setTheme(theme === 'dark' ? 'light' : 'dark')
 }
 
-export function DarkButton(){
+const [theme, setTheme] = useState('light');
+
     return(
         <Button variant="ghost" size="icon" onClick={toggleTheme} className="text-neutral-500">
             {theme === 'dark' ? <Sun className="h-[1.4rem] w-[1.4rem]" /> : <Moon className="h-[1.5rem] w-[1.5rem]" />}
