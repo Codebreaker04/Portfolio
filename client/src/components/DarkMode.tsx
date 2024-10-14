1f99c50ee352931949211717303baeb4fa2fcb3c
yuvraj <yuvrajahuja4@gmail.com>
2024-10-14 14:28:20 +0530

    fixed devolopment error

--- client/src/components/DarkMode.tsx
+++ client/src/components/DarkMode.tsx
@@ -1,10 +1,12 @@
 import {Button} from "@/components/ui/button.tsx";
 import {Sun, Moon} from "lucide-react";
+import {useState} from "react";
+
+const [theme, setTheme] = useState('light');
 
 const toggleTheme = () => {
     setTheme(theme === 'dark' ? 'light' : 'dark')
 }
-let theme = 'light'
 
 export function DarkButton(){
     return(
